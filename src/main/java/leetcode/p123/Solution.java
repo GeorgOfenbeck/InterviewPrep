2a6d3b398f31d3e08d6f6a4143bc0607a4a9af98
Georg Ofenbeck <georg.ofenbeck@swisscom.com>
2020-02-07 14:18:32 +0100

    .

--- src/main/java/leetcode/p123/Solution.java
+++ src/main/java/leetcode/p123/Solution.java
@@ -1,37 +1,39 @@
 package leetcode.p123;
 
 
-
 import java.util.*;
 
 
-abstract class POI{
+abstract class POI {
     int x;
     int pos;
 }
 
-class Min extends POI{
-    Min(int x, int pos){
-        this.pos = pos;
-        this.x = x;
-    }
-}
-class Max extends POI{
-    Max(int x, int pos){
+class Min extends POI {
+    Min(int x, int pos) {
         this.pos = pos;
         this.x = x;
     }
 }
 
-class Profit implements Comparable<Profit>{
+class Max extends POI {
+    Max(int x, int pos) {
+        this.pos = pos;
+        this.x = x;
+    }
+}
+
+class Profit implements Comparable<Profit> {
     Min min;
     Max max;
     int profit;
-    Profit( Min min, Max max){
+
+    Profit(Min min, Max max) {
         this.min = min;
         this.max = max;
         profit = max.x - min.x;
     }
+
     // Overriding equals() to compare two Complex objects
     @Override
     public boolean equals(Object o) {
@@ -59,61 +61,62 @@ class Profit implements Comparable<Profit>{
     public int compareTo(Profit o) {
         if (o.profit < this.profit)
             return -1;
-        else
-        if (o.profit > this.profit)
+        else if (o.profit > this.profit)
             return 1;
-            else
-                if (o.max.pos < this.max.pos)
-                    return 1;
-                else
-                    if (o.max.pos == this.max.pos)
-                        return 0;
-                    else
-                        return -1;
+        else if (o.max.pos < this.max.pos)
+            return 1;
+        else if (o.max.pos == this.max.pos)
+            return 0;
+        else
+            return -1;
     }
 }
 
-class Result{
+class Result {
 
-    Result(int size){
+    Result(int size) {
         k = size;
         sort = new TreeSet<>();
     }
+
     int k = 0;
     SortedSet<Profit> sort;
     SortedSet<Profit> bsort;
     Profit last;
-    int profit(){
+
+    Profit lastnonadded = null;
+
+    int profit() {
         int sum = 0;
         for (Profit p : sort)
             sum += p.profit;
         return sum;
     }
 
-    int oldprofit(){
+    int oldprofit() {
         int sum = 0;
         for (Profit p : bsort)
             sum += p.profit;
         return sum;
     }
 
-    int profitMaxK(Profit px){
+    int profitMaxK(Profit px) {
         sort.add(px);
         int sum = 0;
         for (Profit p : sort)
             sum += p.profit;
-        sum = sum- sort.last().profit; //remove smallest
+        sum = sum - sort.last().profit; //remove smallest
         sort.remove(px);
         if (sort.size() > this.k)
             System.out.println("?");
-        return sum ;
+        return sum;
     }
 
-    Profit getLast(){
+    Profit getLast() {
         return last;
     }
 
-    int profitMaxKwoLast(Profit px){
+    int profitMaxKwoLast(Profit px) {
         if (sort.size() > this.k)
             System.out.println("?");
         if (last != null) sort.remove(last);
@@ -126,20 +129,36 @@ class Result{
         return sum + px.profit;
     }
 
-    void addSetandResult(Result other, Profit px){
+    int profitMaxKwLastNonAddedMin(Profit px){
+        if (lastnonadded == null) return Integer.MIN_VALUE;
+        Profit merge = new Profit(lastnonadded.min,px.max);
+        sort.add(merge);
+        int sum = 0;
+        for (Profit p : sort)
+            sum += p.profit;
+        sum = sum - sort.last().profit; //remove smallest
+        sort.remove(merge);
+        if (sort.size() > this.k)
+            System.out.println("?");
+        return sum;
+    }
+
+
+    void addSetandResult(Result other, Profit px) {
         sort.clear();
-        for (Profit p : other.bsort){
+        for (Profit p : other.bsort) {
             sort.add(p);
         }
         boolean x = sort.add(px);
         if (!x)
             System.out.println("?");
         last = px;
+        lastnonadded = null;
         if (sort.size() > this.k || (last != null && !sort.contains(last)))
             System.out.println("?");
     }
 
-    void add(Profit px){
+    void add(Profit px) {
         boolean x = sort.add(px);
         if (!x)
             System.out.println("?");
@@ -149,22 +168,29 @@ class Result{
             if (toremove != px)
                 last = px;
             sort.remove(toremove); //remove smallest
+
+            if (toremove == px) {
+                if (px.min.x < lastnonadded.min.x)
+                    lastnonadded = px;
+            } else
+                lastnonadded = null;
         }
         if (sort.size() > this.k || (last != null && !sort.contains(last)))
             System.out.println("?");
     }
 
-    void removeLastandAdd(Profit px){
+    void removeLastandAdd(Profit px) {
         if (last != null) sort.remove(last);
         boolean x = sort.add(px);
         if (!x)
             System.out.println("?");
         last = px;
+        lastnonadded = null;
         if (sort.size() > this.k || (last != null && !sort.contains(last)))
             System.out.println("?");
     }
 
-    void backup(){
+    void backup() {
         bsort = new TreeSet<>();
         bsort.addAll(sort);
     }
@@ -173,7 +199,7 @@ class Result{
 
 
 public class Solution {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         //int [] test = new int[]{1,2,-10,4,-20,5};//
         //int [] test = new int[]{3,3,5,0,0,3,1,4};[1,1,0]
         //int [] test = new int[]{2,1,2,0,1,0,10};
@@ -181,7 +207,7 @@ public class Solution {
         //int [] test = new int[]{5,5,4,9,3,8,5,5,1,6,8,3,4};
         //int [] test = new int[]{397,6621,4997,7506,8918,1662,9187,3278,3890,514,18,9305,93,5508,3031,2692,6019,1134,1691,4949,5071,799,8953,7882,4273,302,6753,4657,8368,3942,1982,5117,563,3332,2623,9482,4994,8163,9112,5236,5029,5483,4542,1474,991,3925,4166,3362,5059,5857,4663,6482,3008,3616,4365,3634,270,1118,8291,4990,1413,273,107,1976,9957,9083,7810,4952,7246,3275,6540,2275,8758,7434,3750,6101,1359,4268,5815,2771,126,478,9253,9486,446,3618,3120,7068,1089,1411,2058,2502,8037,2165,830,7994,1248,4993,9298,4846,8268,2191,3474,3378,9625,7224,9479,985,1492,1646,3756,7970,8476,3009,7457,8922,2980,577,2342,4069,8341,4400,2923,2730,2917,105,724,518,5098,6375,5364,3366,8566,8838,3096,8191,2414,2575,5528,259,573,5636,4581,9049,4998,2038,4323,7978,8968,6665,8399,7309,7417,1322,6391,335,1427,7115,853,2878,9842,2569,2596,4760,7760,5693,9304,6526,8268,4832,6785,5194,6821,1367,4243,1819,9757,4919,6149,8725,7936,4548,2386,5354,2222,8777,2041,1,2245,9246,2879,8439,1815,5476,3200,5927,7521,2504,2454,5789,3688,9239,7335,6861,6958,7931,8680,3068,2850,1181,1793,7138,2081,532,2492,4303,5661,885,657,4258,131,9888,9050,1947,1716,2250,4226,9237,1106,6680,1379,1146,2272,8714,8008,9230,6645,3040,2298,5847,4222,444,2986,2655,7328,1830,6959,9341,2716,3968,9952,2847,3856,9002,1146,5573,1252,5373,1162,8710,2053,2541,9856,677,1256,4216,9908,4253,3609,8558,6453,4183,5354,9439,6838,2682,7621,149,8376,337,4117,8328,9537,4326,7330,683,9899,4934,2408,7413,9996,814,9955,9852,1491,7563,421,7751,1816,4030,2662,8269,8213,8016,4060,5051,7051,1682,5201,5427,8371,5670,3755,7908,9996,7437,4944,9895,2371,7352,3661,2367,4518,3616,8571,6010,1179,5344,113,9347,9374,2775,3969,3939,792,4381,8991,7843,2415,544,3270,787,6214,3377,8695,6211,814,9991,2458,9537,7344,6119,1904,8214,6087,6827,4224,7266,2172,690,2966,7898,3465,3287,1838,609,7668,829,8452,84,7725,8074,871,3939,7803,5918,6502,4969,5910,5313,4506,9606,1432,2762,7820,3872,9590,8397,1138,8114,9087,456,6012,8904,3743,7850,9514,7764,5031,4318,7848,9108,8745,5071,9400,2900,7341,5902,7870,3251,7567,2376,9209,9000,1491,7030,2872,7433,1779,362,5547,7218,7171,7911,2474,914,2114,8340,8678,3497,2659,2878,2606,7756,7949,2006,656,5291,4260,8526,4894,1828,7255,456,7180,8746,3838,6404,6179,5617,3118,8078,9187,289,5989,1661,1204,8103,2,6234,7953,9013,5465,559,6769,9766,2565,7425,1409,3177,2304,6304,5005,9559,6760,2185,4657,598,8589,836,2567,1708,5266,1754,8349,1255,9767,5905,5711,9769,8492,3664,5134,3957,575,1903,3723,3140,5681,5133,6317,4337,7789,7675,3896,4549,6212,8553,1499,1154,5741,418,9214,1007,2172,7563,8614,8291,3469,677,4413,1961,4341,9547,5918,4916,7803,9641,4408,3484,1126,7078,7821,8915,1105,8069,9816,7317,2974,1315,8471,8715,1733,7685,6074,257,5249,4688,8549,5070,5366,2962,7031,6059,8861,9301,7328,6664,5294,8088,6500,6421,1518,4321,5336,2623,8742,1505,9941,1716,2820,4764,6783,906,2450,2857,7515,4051,7546,2416,9121,9264,1730,6152,1675,592,1805,9003,7256,7099,3444,3757,9872,4962,4430,1561,7586,3173,3066,3879,1241,2238,8643,8025,3144,7445,882,7012,1496,4780,9428,617,396,1159,3121,2072,1751,4926,7427,5359,8378,871,5468,8250,5834,9899,9811,9772,9424,2877,3651,7017,5116,8646,5042,4612,6092,2277,1624,7588,3409,1053,8206,3806,8564,7679,2230,6667,8958,6009,2026,7336,6881,3847,5586,9067,98,1750,8839,9522,4627,8842,2891,6095,7488,7934,708,3580,6563,8684,7521,9972,6089,2079,130,4653,9758,2360,1320,8716,8370,9699,6052,1603,3546,7991,670,3644,6093,9509,9518,7072,4703,2409,3168,2191,6695,228,2124,3258,5264,9645,9583,1354,1724,9713,2359,1482,8426,3680,6551,3148,9731,8955,4751,9629,6946,5421,9625,9391,1282,5495,6464,5985,4256,5984,4528,952,6212,6652,562,1476,6297,145,9182,8021,6211,1542,5856,4637,1574,2407,7785,1305,1362,2536,934,4661,4309,559,4052,1943,2406,516,4280,6662,2852,8808,7614,9064,1813,4529,6893,8110,4674,2427,2484,7237,3969,8340,1874,5543,7099,6011,3200,8461,8547,486,9474,9208,7397,9879,7503,9803,6747,1783,6466,9600,6944,432,8664,8757,4961,1909,6867,5988,4337,5703,3225,4658,4043,1452,6554,1142,7463,9754,5956,2363,241,1782,7923,7638,1661,5427,3794,8409,7210,260,8009,4154,692,3025,9263,2006,4935,2483,7994,5624,8186,7571,282,8582,9023,6836,6076,6487,6591,2032,8850,3184,3815,3125,7174,5476,8552,968,3885,2115,7580,8246,2621,4625,1272,1885,6631,6207,4368,4625,8183,2554,8548,8465,1136,7572,1654,7213,411,4597,5597,5613,7781,5764,8738,1307,7593,7291,8628,7830,9406,6208,6077,2027,833,7349,3912,7464,9908,4632,8441,8091,7187,6990,2908,4675,914,4562,8240,1325,9159,190,6938,3292,5954,2028,4600,9899,9319,3228,7730,5077,9436,159,7105,6622,7508,7369,4086,3768,2002,8880,8211,5541,2222,1119,216,3136,5682,4809,813,1193,4999,4103,4486,7305,6131,9086,7205,5451,2314,1287,528,8102,1446,3985,4724,5306,1355,5163,9074,9709,4043,7285,5250,2617,4756,1818,2105,6790,6627,2918,7984,7978,7021,2470,1636,3152,7908,8841,4955,222,6480,5484,4676,7926,5821,9401,3232,7176,916,8658,3237,1311,5943,8487,3928,7051,306,6033,3842,3285,8951,1826,7616,2324,648,9252,5476,8556,4445,6784};
         //int [] test = new int[]{-1, 2147483647};
-        int [] test = new int [] {0, 4, 0, 25, 12, 15, 14, 18};
+        int[] test = new int[]{0, 4, 0, 25, 12, 15, 14, 18};
         Solution sol = new Solution();
         System.out.println(sol.maxProfit(test));
         System.out.println(sol.maxProfitn2(test));
@@ -191,26 +217,26 @@ public class Solution {
 
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
-        return maxProfit(prices,2);
+        return maxProfit(prices, 2);
         //return maxProfitn2(prices);
     }
 
-    public void printExtremas(ArrayList<Profit> profits){
-        for (Profit p : profits){
+    public void printExtremas(ArrayList<Profit> profits) {
+        for (Profit p : profits) {
             System.out.print(p.min.x + " ^ " + p.max.x + " ");
         }
         System.out.println();
     }
 
 
-    public int maxProfitn2(int [] prices){
+    public int maxProfitn2(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
-        int [] pricesNoDup = removeDups(prices);
+        int[] pricesNoDup = removeDups(prices);
         int max = 0;
         int j = 0;
-        for (int i = 0; i< pricesNoDup.length; i++){
-            int a = maxProfit1(pricesNoDup,0,i);
-            int b = maxProfit1(pricesNoDup,i,pricesNoDup.length);
+        for (int i = 0; i < pricesNoDup.length; i++) {
+            int a = maxProfit1(pricesNoDup, 0, i);
+            int b = maxProfit1(pricesNoDup, i, pricesNoDup.length);
             int c = a + b;
             if (c > max) {
                 j = i;
@@ -223,17 +249,17 @@ public class Solution {
         return max;
     }
 
-    public int maxProfit1(int [] prices, int start, int end){
+    public int maxProfit1(int[] prices, int start, int end) {
         int min = prices[start];
         int max = prices[start];
         int profit = 0;
-        for (int i = start; i < end; i++){
+        for (int i = start; i < end; i++) {
             int cur = prices[i];
-            if (cur <= min){
+            if (cur <= min) {
                 min = cur;
                 max = min;
             }
-            if (cur > max){
+            if (cur > max) {
                 max = cur;
                 int localprof = max - min;
                 if (localprof > profit)
@@ -244,21 +270,21 @@ public class Solution {
     }
 
     public int maxProfit(int[] prices, int k) {
-        int [] pricesNoDup = removeDups(prices);
+        int[] pricesNoDup = removeDups(prices);
         ArrayList<Profit> profits = sanitize(pricesNoDup);
         printExtremas(profits);
         if (profits.isEmpty()) return 0;
         else {
             ArrayList<Result> results = new ArrayList<>();
-            for (int i = 0 ; i < k; i ++){
-                results.add(new Result(i+1));
+            for (int i = 0; i < k; i++) {
+                results.add(new Result(i + 1));
             }
-            for (int i = 0; i < profits.size(); i++){
+            for (int i = 0; i < profits.size(); i++) {
                 updateFront(results, profits.get(i));
             }
 
             int max = Integer.MIN_VALUE;
-            for (Result r : results){
+            for (Result r : results) {
                 if (r.profit() > max)
                     max = r.profit();
             }
@@ -268,38 +294,37 @@ public class Solution {
 
     }
 
-    private Profit tmax(Profit p1, Profit p2, Profit p3){
-        if (p1.profit >= p2.profit){
+    private Profit tmax(Profit p1, Profit p2, Profit p3) {
+        if (p1.profit >= p2.profit) {
             if (p1.profit >= p3.profit)
                 return p1;
             else
                 return p3;
-        } else
-        if (p2.profit >= p3.profit)
+        } else if (p2.profit >= p3.profit)
             return p2;
         else
             return p3;
     }
-    private Profit dmax(Profit p1, Profit p2){
-        if (p1.profit >= p2.profit){
+
+    private Profit dmax(Profit p1, Profit p2) {
+        if (p1.profit >= p2.profit) {
             return p1;
         } else return p2;
     }
 
 
-
-    private void updateFront(ArrayList<Result> ks, Profit px){
-        for (Result r : ks){
+    private void updateFront(ArrayList<Result> ks, Profit px) {
+        for (Result r : ks) {
             r.backup(); //make sure we have the old ones
         }
 
 
-        for (int i = 0; i < ks.size(); i++){
+        for (int i = 0; i < ks.size(); i++) {
             Result prev = null;
             Result result = ks.get(i);
             int profitPrevAndPx;
-            if (i > 0){
-                prev = ks.get(i-1);
+            if (i > 0) {
+                prev = ks.get(i - 1);
                 profitPrevAndPx = prev.oldprofit() + px.profit;
             } else {
                 profitPrevAndPx = Integer.MIN_VALUE;
@@ -309,17 +334,19 @@ public class Solution {
 
             Profit last = result.getLast();
             if (last == null) last = px;
-            Profit merge = new Profit(last.min,px.max);
+            Profit merge = new Profit(last.min, px.max);
 
             int profitMaxKwoLast = result.profitMaxKwoLast(merge);
+            int profitMaxKwLastNonAddedMin = result.profitMaxKwLastNonAddedMin(px);
 
-            if (profitMaxK > profitMaxKwoLast){
+
+            if (profitMaxK > profitMaxKwoLast) {
                 if (profitMaxK > profitPrevAndPx) {
                     //profitMaxK
                     result.add(px);
                 } else {
                     //profitPrevAndPx
-                    result.addSetandResult(prev,px);
+                    result.addSetandResult(prev, px);
                 }
             } else {
                 if (profitMaxKwoLast > profitPrevAndPx) {
@@ -327,13 +354,13 @@ public class Solution {
                     result.removeLastandAdd(merge);
                 } else {
                     //profitPrevAndPx
-                    result.addSetandResult(prev,px);
+                    result.addSetandResult(prev, px);
                 }
             }
         }
     }
 
-    private int [] removeDups(int [] prices){
+    private int[] removeDups(int[] prices) {
         if (prices == null) return null;
         if (prices.length < 2) return prices;
         LinkedList<Integer> buffer = new LinkedList<>();
@@ -341,33 +368,33 @@ public class Solution {
         int last = prices[0];
         buffer.add(prices[0]);
         int pos = 1;
-        for (int i = 1; i < prices.length; i++){
+        for (int i = 1; i < prices.length; i++) {
             if (prices[i] != last) {
                 buffer.add(prices[i]);
                 last = prices[i];
             }
         }
-        int [] ret = new int[buffer.size()];
+        int[] ret = new int[buffer.size()];
         pos = 0;
-        for (Integer e : buffer){
+        for (Integer e : buffer) {
             ret[pos] = e;
-            pos ++;
+            pos++;
         }
         return ret;
 
     }
 
 
-    private ArrayList<Profit> sanitize(int [] prices){
-        if (prices == null || prices.length == 0 ) return new ArrayList<>();
+    private ArrayList<Profit> sanitize(int[] prices) {
+        if (prices == null || prices.length == 0) return new ArrayList<>();
 
 
         LinkedList<POI> poi = new LinkedList<>();
-        for (int i = 1; i < prices.length-1; i++){
-            if (prices[i-1] <= prices[i] && prices[i+1] < prices[i])
-                poi.add(new Max(prices[i],i));
-            if (prices[i-1] >= prices[i] && prices[i+1] > prices[i])
-                poi.add(new Min(prices[i],i));
+        for (int i = 1; i < prices.length - 1; i++) {
+            if (prices[i - 1] <= prices[i] && prices[i + 1] < prices[i])
+                poi.add(new Max(prices[i], i));
+            if (prices[i - 1] >= prices[i] && prices[i + 1] > prices[i])
+                poi.add(new Min(prices[i], i));
         }
         if (!poi.isEmpty()) {
             if (poi.getFirst() instanceof Max)
@@ -375,7 +402,7 @@ public class Solution {
             if (poi.getLast() instanceof Min)
                 poi.addLast(new Max(prices[prices.length - 1], prices.length - 1));
         } else {
-            if (prices[0] < prices[prices.length-1]){
+            if (prices[0] < prices[prices.length - 1]) {
                 poi.add(new Min(prices[0], 0));
                 poi.add(new Max(prices[prices.length - 1], prices.length - 1));
             }
@@ -383,12 +410,11 @@ public class Solution {
         ArrayList<Profit> profits = new ArrayList<>();
         if (poi.isEmpty()) return profits;
 
-        for (int i = 0; i < poi.size(); i = i+ 2){
-            if (poi.get(i) instanceof Min && poi.get(i+1) instanceof Max)
-            {
+        for (int i = 0; i < poi.size(); i = i + 2) {
+            if (poi.get(i) instanceof Min && poi.get(i + 1) instanceof Max) {
                 Min min = (Min) poi.get(i);
-                Max max = (Max) poi.get(i+1);
-                Profit profit = new Profit(min,max);
+                Max max = (Max) poi.get(i + 1);
+                Profit profit = new Profit(min, max);
                 profits.add(profit);
             }
         }
